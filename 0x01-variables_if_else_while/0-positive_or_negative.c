1b528376bfd3b215756e81d5ee37c25c9b42c8d1
Law317git <lawalolarewaju110@gmail.com>
2023-06-17 05:06:06 -0700

    2nd commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,16 +18,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("is positive\n");
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
 
-		printf("is zero\n");
+		printf("%d is zero\n, n");
 	}
 	else
 	{
-		printf("is negative\n");
+		printf("is negative\n, n");
 	}
 	return (0);
 }
